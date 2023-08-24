992e29b3ecced90c7a72a4d8ea39fbd049789d16
navjotsingh3930 <131993694+navjotsingh3930@users.noreply.github.com>
2023-08-24 09:22:56 +0530

    navbar created

--- layout/Footer/Footer.js
+++ layout/Footer/Footer.js
@@ -1,9 +1,37 @@
 import React from 'react'
 import styles from './footer.module.css'
+import { FaBeer } from 'react-icons/fa';
+import {AiOutlineFacebook} from 'react-icons/ai';
+import {AiOutlineYoutube} from 'react-icons/ai';
+import {SiIndeed} from 'react-icons/si';
+import {BsInstagram} from 'react-icons/bs';
+import Link from 'next/link';
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className={styles.container}>
+      <div className={styles.topFooter}>
+         <h1>OUR NETWORK</h1>
+      </div>
+      <div className={styles.middleFooter}>
+       
+      </div>
+      <div className={styles.bottomFooter}>
+       <ul>
+         <li>
+         <AiOutlineFacebook/>
+         </li>
+         <li>
+         <AiOutlineYoutube/>
+         </li>
+         <li>
+          <SiIndeed/>
+         </li>
+         <li>
+          <BsInstagram/>
+         </li>
+       </ul>
+       <p>The Gym © 2019 Tucson Fitness Pvt. Ltd.</p>
+      </div>
     </div>
   )
 }
--- layout/Header/Header.js
+++ layout/Header/Header.js
@@ -1,10 +1,40 @@
 import React from 'react'
 import styles from './header.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+      <div className={styles.Container}>
+        <div className={styles.logoContainer}>
+          <Image src={'/thegym-logo.jpg'} alt='jaspreet is hero' width={500} height={500}/>
+        </div>
+        <ul className={styles.links}>
+          <li>
+            <Link href={'/'}>HOME</Link>
+          </li>
+          <li>
+            <Link href={'/about'}>ABOUT</Link>
+          </li>
+          <li>
+            <Link href={'/find_the_gym'}>FIND THE GYM</Link>
+          </li>
+          <li>
+            <Link href={'/classes'}>CLASSES</Link>
+          </li>
+          <li>
+            <Link href={'/membership'}>MEMBERSHIP</Link>
+          </li>
+          <li>
+            <Link href={'/franchise'}>FRANCHISE</Link>
+          </li>
+          <li>
+            <Link href={'/faq'}>FAQ</Link>
+          </li>
+          <li>
+            <Link href={'/contact'}>CONTACT</Link>
+          </li>
+        </ul>
+      </div>
   )
 }
 
